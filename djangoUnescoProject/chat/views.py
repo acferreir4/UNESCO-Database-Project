062c7ffc136bef2a2ff81435de17c87bb1fb6f77
DhruboCSE <aaa.dhrubo@gmail.com>
2019-03-04 19:00:11 -0500

    Added chat download and AC feature

--- djangoUnescoProject/chat/views.py
+++ djangoUnescoProject/chat/views.py
@@ -1,7 +1,10 @@
+import csv
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils.safestring import mark_safe
 from .models import ChatRooms, RoomAccess
+from .models import Message
 import json
 
 def index(request):
@@ -10,15 +13,49 @@ def index(request):
 @login_required
 def room(request, room_name):
 
-    return render(request, 'chat/room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name)),
-        'username': mark_safe(json.dumps(request.user.username)),
-        'chatRooms': ChatRooms.objects.all(),
-        'roomAccess': RoomAccess.objects.all(),
-    })
+	valid = False;
+	for valid_rooms in RoomAccess.objects.filter(user=request.user):
+		if valid_rooms.roomName.name == room_name:
+			valid = True;
 
+	if request.user.is_staff:
+		valid = True;
+
+	if valid:
+		return render(request, 'chat/room.html', {
+	        'room_name_json': mark_safe(json.dumps(room_name)),
+	        'username': mark_safe(json.dumps(request.user.username)),
+	        'chatRooms': ChatRooms.objects.order_by('category').all(),
+	        'roomAccess': RoomAccess.objects.all(),
+	    })
+	else:
+		return render(request, 'chat/accessDenied.html')
 # def list(request):
 #     context = {
 #         'chatRooms': ChatRooms.objects.all()
 #     }
 #     return render(request, 'chat/room.html', context)
+
+def downloadChat(request, room_name):
+    if not request.user.is_staff:
+        return render(request, 'chat/accessDenied.html')
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="chat-transcript.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow([
+        'Author', 
+        'Timestamp', 
+        'Content',
+        ])
+
+    rm = ChatRooms.objects.filter(name=room_name).first()
+    for msg in Message.objects.filter(chatRoom=rm).order_by('timestamp'):
+        writer.writerow([
+            msg.author, 
+            msg.timestamp, 
+            msg.content,
+            ])
+
+    return response
--- djangoUnescoProject/users/signals.py
+++ djangoUnescoProject/users/signals.py
@@ -9,9 +9,11 @@ def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
         room = ChatRooms.objects.create(name=instance.username, 
-        	category='P', display_line_1=instance.get_full_name())
+        	category='P', display_line_1=instance.get_full_name(), 
+        	display_line_2=instance.institution)
         RoomAccess.objects.create(user=instance,roomName=room)
-
+        main_room = ChatRooms.objects.filter(name='Main_Rooom').first()
+        RoomAccess.objects.create(user=instance,roomName=main_room)
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
