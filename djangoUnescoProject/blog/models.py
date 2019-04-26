96fbd3d198e7a0e60e284febdb677f5da2f14bdc
Asma <asma.hassan@ibm.com>
2019-04-25 22:47:37 -0400

    Functional expiry date

--- djangoUnescoProject/blog/models.py
+++ djangoUnescoProject/blog/models.py
@@ -2,15 +2,19 @@ from django.db import models
 from django.utils import timezone
 from users.models import User
 from django.urls import reverse
+from datetime import date
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    date_expired = models.DateField('Expiry Date (YYYY-MM-DD)')
+    date_expired = models.DateTimeField('Expiry Date (YYYY-MM-DD)')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     fileAttachment = models.FileField('File Attachment', upload_to='announcements/files', blank=True, null=True)
     imageAttachment = models.ImageField('Image Attachment',upload_to='announcements/images', blank=True, null=True)
+
+    def is_future_event(self):
+        return self.date_expired.date() > date.today()
 
     
     def __str__(self):
