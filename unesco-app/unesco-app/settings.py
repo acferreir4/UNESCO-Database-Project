ae639e14717a26d7f884f83001c22e78bfa128c5
Andrew Ferreira <acferreir4>
2019-08-18 18:56:53 +0000

    Update requirements

--- unesco-app/unesco-app/settings.py
+++ unesco-app/unesco-app/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = '=&p=5dbl*=n4i4edb&vimog!4si)fl5kj6y@(khj*%7gw$e_b_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['159.89.124.218', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['68.183.199.248', '127.0.0.1', 'localhost']
 
 # Application definition
 
@@ -93,13 +93,24 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'TEST': {
+#            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
+#        }
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'TEST': {
-            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
-        }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'unescodb',
+        'USER': 'unesco_admin',
+        'PASSWORD': 'indigenous123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -141,6 +152,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'chat/static'),
     os.path.join(BASE_DIR, 'dynamicforms/static')
