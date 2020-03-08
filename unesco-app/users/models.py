173c046d62bb5efe4209bb30ce58e481a0d0cf6a
Andrew Ferreira <acferreir4@gmail.com>
2020-03-08 22:39:41 +0000

    Allow phone number field to be blank

--- unesco-app/users/models.py
+++ unesco-app/users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
 
 class User(AbstractUser):
-    phone_number = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20, blank=True)
     #phone_number = PhoneNumberField(
             #'Phone number', 
             #blank=True, 
